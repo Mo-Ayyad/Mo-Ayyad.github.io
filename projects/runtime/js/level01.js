64fd03bf8d5d68bff21ab1fdf679de3e3d635edb
Mo-Ayyad <m.ayyad@ardencahillacademy.com>
2023-01-11 16:12:28 +0000

    fixed function calls

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -16,114 +16,115 @@ var level01 = function (window) {
             "number": 1,
             "speed": -3,
             "gameItems": [
-                { type: "sawBlade", x: 400, y: groundY - 240 },
-                { type: "sawBlade", x: 2100, y: groundY - 320 },
-                { type: "sawBlade", x: 1000, y: groundY - 240 },
+                { type: "sawBlade", x: 400, y: groundY - 10 },
+                { type: "sawBlade", x: 2500, y: groundY - 20 },
+                { type: "sawBlade", x: 3100, y: groundY - 120 },
                 { type: "bBall", x: 2000, y: groundY - 10 },
                 { type: "bBall", x: 800, y: groundY - 120 },
-                { type: "bBall", x: 1500, y: groundY - 50 },
-                { type: "enemy", x: 2000, y: groundY - 10 },
+                { type: "bBall", x: 1500, y: groundY - 10 },
+                { type: "enemy", x: 2900, y: groundY - 50 },
                 { type: "enemy", x: 800, y: groundY - 120 },
-                { type: "enemy", x: 1500, y: groundY - 50 },
-                { type: "reward", x: 500, y: groundY - 10 },
-                { type: "reward", x: 300, y: groundY - 120 },
+                { type: "enemy", x: 1400, y: groundY - 50 },
+                { type: "reward", x: 3200, y: groundY - 120 },
+                { type: "reward", x: 4000, y: groundY - 120 },
                 { type: "reward", x: 1200, y: groundY - 50 },
             ]
         };
 
-        // for (var i = 0; i < levelData.gameItems.length; i++) {
-        //     var gameItem = levelData.gameItems[i];
-        //     if (gameItem.type === "sawBlade") {
-        //         createSawblade(gameItem.x, gameItem.y);
-        //     } else if (gameItem.type === "bBall") {
-        //         createObstacle(gameItem.x, gameItem.y);
-        //     } else if (gameItem.type === "enemy") {
-        //         createEnemy(gameItem.x, gameItem.y);
-        //     } else if (gameItem.type === "reward") {
-        //         createReward(gameItem.x, gameItem.y);
-        //     }
-        // };
+        for (var i = 0; i <= levelData.gameItems.length; i++) {
+            var gameItem = levelData.gameItems[i];
+            if (gameItem.type === "sawBlade") {
+                createSawBlade(gameItem.x, gameItem.y);
+            } else if (gameItem.type === "bBall") {
+                createBBall(gameItem.x, gameItem.y);
+            } else if (gameItem.type === "enemy") {
+                createEnemy(gameItem.x, gameItem.y);
+            } else if (gameItem.type === "reward") {
+                createReward(gameItem.x, gameItem.y);
+            }
+        }
 
 
 
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // TODO 6 and on go here
         // BEGIN EDITING YOUR CODE HERE
 
+        function createSawBlade(x, y) {
+            var hitZoneSize = 25;
+            var damageFromObstacle = 10;
+            var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
+            sawBladeHitZone.x = x;
+            sawBladeHitZone.y = y;
+            game.addGameItem(sawBladeHitZone);
+            var obstacleImage = draw.bitmap("img/sawblade.png");
+            obstacleImage.x = -25;
+            obstacleImage.y = -25;
+            sawBladeHitZone.addChild(obstacleImage);
+        };
 
-        var hitZoneSize = 25;
-        var damageFromObstacle = 10;
-        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
-        sawBladeHitZone.x = 2100;
-        sawBladeHitZone.y = 240;
-        game.addGameItem(sawBladeHitZone);
-        var obstacleImage = draw.bitmap("img/sawblade.png");
-        obstacleImage.x = -25;
-        obstacleImage.y = -25;
-        sawBladeHitZone.addChild(obstacleImage);
+        // var hitZoneSize = 25;
+        // var damageFromObstacle = 10;
+        // var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
+        // sawBladeHitZone.x = 2400;
+        // sawBladeHitZone.y = 320;
+        // game.addGameItem(sawBladeHitZone);
+        // var obstacleImage = draw.bitmap("img/sawblade.png");
+        // obstacleImage.x = -25;
+        // obstacleImage.y = -25;
+        // sawBladeHitZone.addChild(obstacleImage);
 
-        var hitZoneSize = 25;
-        var damageFromObstacle = 10;
-        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
-        sawBladeHitZone.x = 2400;
-        sawBladeHitZone.y = 320;
-        game.addGameItem(sawBladeHitZone);
-        var obstacleImage = draw.bitmap("img/sawblade.png");
-        obstacleImage.x = -25;
-        obstacleImage.y = -25;
-        sawBladeHitZone.addChild(obstacleImage);
-
-        var hitZoneSize = 25;
-        var damageFromObstacle = 10;
-        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
-        sawBladeHitZone.x = 1700;
-        sawBladeHitZone.y = 240;
-        game.addGameItem(sawBladeHitZone);
-        var obstacleImage = draw.bitmap("img/sawblade.png");
-        obstacleImage.x = -25;
-        obstacleImage.y = -25;
-        sawBladeHitZone.addChild(obstacleImage);
-
-
-        var hitZoneSize = 25;
-        var damageFromObstacle = 10;
-        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
-        sawBladeHitZone.x = 5100;
-        sawBladeHitZone.y = 240;
-        game.addGameItem(sawBladeHitZone);
-        var obstacleImage = draw.bitmap("img/sawblade.png");
-        obstacleImage.x = -25;
-        obstacleImage.y = -25;
-        sawBladeHitZone.addChild(obstacleImage);
-
-        var hitZoneSize = 25;
-        var damageFromObstacle = 10;
-        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
-        sawBladeHitZone.x = 5400;
-        sawBladeHitZone.y = 320;
-        game.addGameItem(sawBladeHitZone);
-        var obstacleImage = draw.bitmap("img/sawblade.png");
-        obstacleImage.x = -25;
-        obstacleImage.y = -25;
-        sawBladeHitZone.addChild(obstacleImage);
-
-        var hitZoneSize = 25;
-        var damageFromObstacle = 10;
-        var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
-        sawBladeHitZone.x = 4700;
-        sawBladeHitZone.y = 240;
-        game.addGameItem(sawBladeHitZone);
-        var obstacleImage = draw.bitmap("img/sawblade.png");
-        obstacleImage.x = -25;
-        obstacleImage.y = -25;
-        sawBladeHitZone.addChild(obstacleImage);
+        // var hitZoneSize = 25;
+        // var damageFromObstacle = 10;
+        // var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
+        // sawBladeHitZone.x = 1700;
+        // sawBladeHitZone.y = 240;
+        // game.addGameItem(sawBladeHitZone);
+        // var obstacleImage = draw.bitmap("img/sawblade.png");
+        // obstacleImage.x = -25;
+        // obstacleImage.y = -25;
+        // sawBladeHitZone.addChild(obstacleImage);
 
 
+        // var hitZoneSize = 25;
+        // var damageFromObstacle = 10;
+        // var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
+        // sawBladeHitZone.x = 5100;
+        // sawBladeHitZone.y = 240;
+        // game.addGameItem(sawBladeHitZone);
+        // var obstacleImage = draw.bitmap("img/sawblade.png");
+        // obstacleImage.x = -25;
+        // obstacleImage.y = -25;
+        // sawBladeHitZone.addChild(obstacleImage);
 
-        function createBball(x, y) {
+        // var hitZoneSize = 25;
+        // var damageFromObstacle = 10;
+        // var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
+        // sawBladeHitZone.x = 5400;
+        // sawBladeHitZone.y = 320;
+        // game.addGameItem(sawBladeHitZone);
+        // var obstacleImage = draw.bitmap("img/sawblade.png");
+        // obstacleImage.x = -25;
+        // obstacleImage.y = -25;
+        // sawBladeHitZone.addChild(obstacleImage);
+
+        // var hitZoneSize = 25;
+        // var damageFromObstacle = 10;
+        // var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle)
+        // sawBladeHitZone.x = 4700;
+        // sawBladeHitZone.y = 240;
+        // game.addGameItem(sawBladeHitZone);
+        // var obstacleImage = draw.bitmap("img/sawblade.png");
+        // obstacleImage.x = -25;
+        // obstacleImage.y = -25;
+        // sawBladeHitZone.addChild(obstacleImage);
+
+
+
+        function createBBall(x, y) {
             var hitZoneSize = 25;
             var damageFromObstacle = 10;
             var bballHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
@@ -137,12 +138,12 @@ var level01 = function (window) {
         }
 
 
-        createBball(400, 240);
-        createBball(750, 320);
-        createBball(1000, 240);
-        createBball(3400, 240);
-        createBball(3750, 320);
-        createBball(4000, 240);
+        // createBball(400, 240);
+        // createBball(750, 320);
+        // createBball(1000, 240);
+        // createBball(3400, 240);
+        // createBball(3750, 320);
+        // createBball(4000, 240);
 
         function createEnemy(x, y) {
             var enemy = game.createGameItem("enemy", 25);
@@ -164,12 +165,12 @@ var level01 = function (window) {
             }
         }
 
-        createEnemy(2000, groundY - 10);
-        createEnemy(800, groundY - 120);
-        createEnemy(1500, groundY - 50);
-        createEnemy(6000, groundY - 10);
-        createEnemy(4800, groundY - 120);
-        createEnemy(5500, groundY - 50);
+        // createEnemy(2000, groundY - 10);
+        // createEnemy(800, groundY - 120);
+        // createEnemy(1500, groundY - 50);
+        // createEnemy(6000, groundY - 10);
+        // createEnemy(4800, groundY - 120);
+        // createEnemy(5500, groundY - 50);
 
 
         function createReward(x, y) {
@@ -193,8 +194,8 @@ var level01 = function (window) {
             }
         }
 
-        createReward(2500, groundY - 50);
-        createReward(6500, groundY - 50);
+        // createReward(2500, groundY - 50);
+        // createReward(6500, groundY - 50);
 
 
 
