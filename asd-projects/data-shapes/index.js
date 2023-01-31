ee8b5b49644879f3931fde8cdcc8a97441875b2d
Mo-Ayyad <m.ayyad@ardencahillacademy.com>
2023-01-31 15:43:55 +0000

    added new project

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -36,21 +36,50 @@ $(document).ready(function () {
   /////////////////////////////////////////////////
   // ALL OF YOUR CODE SHOULD GO BELOW HERE ////////
   /////////////////////////////////////////////////
+  // todo "// TODO 0 complete"
+
 
   // TODO 1: create a new shape object and add it to the array
-  
+
+  shape = {
+    "color": "blue",
+    "shape": "circle",
+    "repeat": 3
+  }
+  dataShapes.push(shape);
 
   // TODO 2: add a new property to all data shapes
-  
+  for (i = 0; i < dataShapes.length; i++) {
+    currentShape = dataShapes[i];
+    if (currentShape.color === "red") {
+      currentShape.goodBehavior = "bounce";
+    }
+    else if (currentShape.color === "blue") {
+      currentShape.goodBehavior = "blink";
+    }
+    else {
+      currentShape.goodBehavior = "spin";
+    }
+  }
 
   // TODO 3-a: add a function that handles the static display type
-  
+  function handleStatic(data) {
+    setBackgroundWithObject(data);
+    animationDetails.displayType = 1;
+  }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGoood(color, shape, repeat) {
+    setBackgroundWithSimple(color, shape, repeat)
+    animationDetails.displayType = 2;
+  }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+function handleBad(data, repeat) {
+  repeat = repeat + 1;
+  setBackgroundWithMixed(data, repeat);
+  animationDetails.displayType = 3;
+}
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -58,17 +87,17 @@ $(document).ready(function () {
 
   function staticDisplay() {
     // TODO 3-b: call your handleStatic function
-    
+    handleStatic(currentShape)
   }
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+   currentShape = 
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+
   }
 
   /////////////////////////////////////////////////
@@ -213,14 +242,14 @@ $(document).ready(function () {
     animationDetails.y += animationDetails.speedY;
     if (
       animationDetails.x + $("#shape").width() + 8 >=
-        $("#shape-container").width() ||
+      $("#shape-container").width() ||
       animationDetails.x < 2
     ) {
       animationDetails.speedX *= -1;
     }
     if (
       animationDetails.y + $("#shape").height() + 4 >=
-        $("#shape-container").height() ||
+      $("#shape-container").height() ||
       animationDetails.y < 2
     ) {
       animationDetails.speedY *= -1;
