2c9a1909fd106e9d8a95aae7a13a234c1bfdcf09
Mo-Ayyad <m.ayyad@ardencahillacademy.com>
2023-04-26 14:49:23 +0000

    pushed

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -13,16 +13,69 @@ The CSS ids you will work with are:
 /////////////////////// YOUR WORK GOES BELOW HERE /////////////////////
 ///////////////////////////////////////////////////////////////////////
 
+// TODO 1: Implement swap
+
+function swap(array, i, j) {
+  const temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+  drawSwap(array, i, j);
+}
+
 // TODO 2: Implement bubbleSort
+
+async function bubbleSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = array.length - 1; j > i; j--) {
+      if (array[j].value < array[j - 1].value) {
+        swap(array, j, j - 1);
+        updateCounter("#bubbleCounter");
+        await sleep();
+      }
+    }
+  }
+}
 
 
 // TODO 3: Implement quickSort
 
 
+async function quicksort(array, left, right) {
+  if (right - left > 0) {
+    index = await partition(array, left, right)
+
+    if (left < index - 1) {
+      await quicksort(array, left, index - 1)
+    }
+    if (index < right) {
+      await quicksort(array, index, right)
+    }
+  }
+
+}
 // TODOs 4 & 5: Implement partition
 
+async function partition(array, left, right) {
+  var pivot = array[Math.floor((right + left) / 2)].value;
+  while (left < right) {
+    // First inner while loop
+    while (array[left].value < pivot) {
+      left += 1;
+    }
+    // Second inner while loop
+    while (array[right].value > pivot) {
+      right -= 1;
+    }
+    if (left < right) {
+      // swap elements
+      swap(array, left, right);
+      updateCounter("#quickCounter");
+      await sleep();
+    }
+  }
 
-// TODO 1: Implement swap
+  return left + 1;
+}
 
 
 ///////////////////////////////////////////////////////////////////////
@@ -32,22 +85,22 @@ The CSS ids you will work with are:
 //////////////////////////// HELPER FUNCTIONS /////////////////////////
 
 // this function makes the program pause by SLEEP_AMOUNT milliseconds whenever it is called
-function sleep(){
-    return new Promise(resolve => setTimeout(resolve, SLEEP_AMOUNT));
+function sleep() {
+  return new Promise(resolve => setTimeout(resolve, SLEEP_AMOUNT));
 }
 
 // This function draws the swap on the screen
-function drawSwap(array, i, j){
-    let element1 = array[i];
-    let element2 = array[j];
+function drawSwap(array, i, j) {
+  let element1 = array[i];
+  let element2 = array[j];
 
-    let temp = parseFloat($(element1.id).css("top")) + "px";
+  let temp = parseFloat($(element1.id).css("top")) + "px";
 
-    $(element1.id).css("top", parseFloat($(element2.id).css("top")) + "px");
-    $(element2.id).css("top", temp);
+  $(element1.id).css("top", parseFloat($(element2.id).css("top")) + "px");
+  $(element2.id).css("top", temp);
 }
 
 // This function updates the specified counter
-function updateCounter(counter){
-    $(counter).text("Move Count: " + (parseFloat($(counter).text().replace(/^\D+/g, '')) + 1));
+function updateCounter(counter) {
+  $(counter).text("Move Count: " + (parseFloat($(counter).text().replace(/^\D+/g, '')) + 1));
 }
